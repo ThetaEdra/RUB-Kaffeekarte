858f67d7eb1a3695cd10e5e68d8e224154684ff8
ThetaEdra <>
2022-08-22 00:08:28 +0200

    The Map is live

---
+++ initmap.js
@@ -0,0 +1,147 @@
+// New map with viewpoint over rub
+var mapid = L.map('mapid').setView([51.4456659, 7.2594206], 16);
+
+// Use the tiles from https://www.openstreetmap.de/karte.html
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
+    'useCache': true
+}).addTo(mapid);
+
+// Show mousecoordinates
+L.control.mousePosition().addTo(mapid);
+
+// What happens when the location of the user was found
+function onLocationFound(e) {
+    // the circle size is the inaccuracy field
+    var radius = e.accuracy / 2;
+    // adds a marker at the users position with a popup text
+    L.marker(e.latlng).addTo(mapid)
+        .bindPopup("Du bist hier +- " + radius / 2 + " Meter");
+    L.circle(e.latlng, radius).addTo(mapid);
+}
+
+// what happens when it wasnt found
+function onLocationError(e) {
+    // We dont want to annoy the users for now
+    //alert("Ich konnte dich leider nicht auf der Karte finden");
+}
+
+// handle the functions and try to locate the user
+mapid.on('locationfound', onLocationFound);
+mapid.on('locationerror', onLocationError);
+// locate the user. Also TODO:
+// Use "watch: true" in the future maybe? Look into the documentation
+// how it is implemented and see if markers can be refreshed easily
+mapid.locate({maxZoom: 16});
+
+
+var markerlist = {};
+// Load Colored Markers
+var blueIcon = new L.Icon({
+    iconUrl: 'img/marker-icon-2x-blue.png',
+    shadowUrl: 'img/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+markerlist['blue'] = blueIcon;
+
+var goldIcon = new L.Icon({
+    iconUrl: 'img/marker-icon-2x-gold.png',
+    shadowUrl: 'img/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+markerlist['gold'] = goldIcon;
+
+var redIcon = new L.Icon({
+    iconUrl: 'img/marker-icon-2x-red.png',
+    shadowUrl: 'img/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+markerlist['red'] = redIcon;
+
+var greenIcon = new L.Icon({
+    iconUrl: 'img/marker-icon-2x-green.png',
+    shadowUrl: 'img/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+markerlist['green'] = greenIcon;
+
+var orangeIcon = new L.Icon({
+    iconUrl: 'img/marker-icon-2x-orange.png',
+    shadowUrl: 'img/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+markerlist['orange'] = orangeIcon;
+
+var yellowIcon = new L.Icon({
+    iconUrl: 'img/marker-icon-2x-yellow.png',
+    shadowUrl: 'img/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+markerlist['yellow'] = yellowIcon;
+
+var violetIcon = new L.Icon({
+    iconUrl: 'img/marker-icon-2x-violet.png',
+    shadowUrl: 'img/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+markerlist['violet'] = violetIcon;
+
+var greyIcon = new L.Icon({
+    iconUrl: 'img/marker-icon-2x-grey.png',
+    shadowUrl: 'img/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+markerlist['grey'] = greyIcon;
+
+var blackIcon = new L.Icon({
+    iconUrl: 'img/marker-icon-2x-black.png',
+    shadowUrl: 'img/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+markerlist['black'] = blackIcon;
+
+var greenIcon = new L.Icon({
+    iconUrl: 'img/marker-icon-2x-green.png',
+    shadowUrl: 'img/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+markerlist['green'] = greenIcon;
+
+for (var i in markers) {
+    // Position
+    var latlng = L.latLng({ lat: markers[i].latitud, lng: markers[i].longitud });
+    // Adding it with the given marker color
+    var m = L.marker(latlng, {icon: markerlist[markers[i].icon]}).addTo(mapid);
+    // Name in bold and description/html underneath that
+    m.bindPopup('<b><p>' + markers[i].name + '</p></b><p>' + markers[i].description + '</p>');
+}
---
+++ markers/markers.js
@@ -0,0 +1,109 @@
+markers=[{
+    "name": "Café Olé 🦊🦊🦊🦊🦊",
+    "icon": "violet",
+    "latitud": 51.44599,
+    "longitud": 7.26042,
+    "description": "(Ordentlich starker) Kaffee für 1,20€, Mo. - Fr. 8:00 - 17:00 Uhr . Viele Zusätze wie Sojamilch oder Sirup verfügbar. Im SSC direkt hinter der Drehtür. Zudem gibt es Brötchen, Toast, Kuchen und weiteres. Die Bedienung ist sehr freundlich."
+},{
+    "name": "Kulturkaffee 🦊🦊🦊🦊🦊",
+    "icon": "violet",
+    "latitud": 51.44586,
+    "longitud": 7.25951,
+    "description": "Hat im Semester von Mo-Do 10-20 Uhr und Fr 10-16 Uhr geöffnet. Man kann sowohl drinnen als auch draußen sitzen. Es gibt zudem Eis, Kuchen, Pizza und weiteres. Schachbretter und Steckdosen sind auch vorhanden. <br><a href='https://kulturcafe.asta-bochum.de/' target='_blank'>Website</a>"
+},{
+    "name": "Q-West 🦊🦊🦊🦊",
+    "icon": "green",
+    "latitud": 51.44409,
+    "longitud": 7.25883,
+    "description": "Der Kaffe ist gut. Aber besonders sind hier die legendären Erdbeertörtchen und lange Öffnungszeiten. Es gibt draußen und drinnen Sitzplätze. Vielleicht ein guter Ort für ein Date?"
+},{
+    "name": "Kaffeebar 🦊🦊🦊",
+    "icon": "green",
+    "latitud": 51.4433,
+    "longitud": 7.26224,
+    "description": "Die Preise und der Kaffee sind okay. Kann je nach Uhrzeit sehr voll sein. Am besten einen Kaffe mit einer Kugel Vanilleeis im Becher bestellen und danach auf die Reling am Bistro setzen. Die Aussicht holt einiges wieder raus."
+},{
+    "name": "Edwards 🦊🦊",
+    "icon": "green",
+    "latitud": 51.4452,
+    "longitud": 7.26054,
+    "description": "Praktischerweise direkt in der Bibliothek. Allerdings gibt es soweit ich weiß nicht wie im KuKaf die Möglichkeit Laptops zu laden. Günstig ist es auch nicht und es hat ein wenig was von Starbucks Vibes."
+},{
+    "name": "Café Insgrüne",
+    "icon": "red",
+    "latitud": 51.44405,
+    "longitud": 7.2599,
+    "description": "Hat vorerst geschlossen. Gerne eine E-Mail schreiben, wenn es etwas neues gibt."
+},{
+    "name": "Beckmanns Hof",
+    "icon": "yellow",
+    "latitud": 51.44208,
+    "longitud": 7.26452,
+    "description": "Fuchsi war hier noch nicht. Gibt es hier guten Kaffee?"
+},{
+    "name": "Botanischer Garten",
+    "icon": "yellow",
+    "latitud": 51.44303,
+    "longitud": 7.26689,
+    "description": "Fuchsi war hier noch nicht. Gibt es hier guten Kaffee?"
+},{
+    "name": "SSC-Cafeteria 🦊🦊",
+    "icon": "green",
+    "latitud": 51.4461,
+    "longitud": 7.26114,
+    "description": "Normaler Mensa Kaffee. Mit einer eigenen Tasse kann man ein wenig sparen."
+},{
+    "name": "IB-Cafeteria 🦊🦊",
+    "icon": "green",
+    "latitud": 51.44591,
+    "longitud": 7.26368,
+    "description": "Normaler Mensa Kaffee. Mit einer eigenen Tasse kann man ein wenig sparen. Zudem gibt es Automaten."
+},{
+    "name": "ID-Cafeteria 🦊🦊",
+    "icon": "green",
+    "latitud": 51.44703,
+    "longitud": 7.2663,
+    "description": "Normaler Mensa Kaffee. Mit einer eigenen Tasse kann man ein wenig sparen und den Chociatto-Button am Automaten kennen nur wenige. Zudem gibt es Automaten."
+},{
+    "name": "GA-Cafeteria",
+    "icon": "yellow",
+    "latitud": 51.44309,
+    "longitud": 7.25973,
+    "description": "Unbekannt ob es hier Kaffee gibt / ob aktuell geöffnet ist. 🦊 würde sich über eine Email freuen."
+},{
+    "name": "GB-Cafeteria",
+    "icon": "yellow",
+    "latitud": 51.44255,
+    "longitud": 7.2588,
+    "description": "Unbekannt ob es hier Kaffee gibt / ob aktuell geöffnet ist. 🦊 würde sich über eine Email freuen."
+},{
+    "name": "GC-Cafeteria 🦊🦊",
+    "icon": "yellow",
+    "latitud": 51.44238,
+    "longitud": 7.25742,
+    "description": "Unbekannt ob es hier Kaffee gibt / ob aktuell geöffnet ist. 🦊 würde sich über eine Email freuen."
+},{
+    "name": "GD-Cafeteria 🦊",
+    "icon": "green",
+    "latitud": 51.44216,
+    "longitud": 7.25624,
+    "description": "Normaler Mensa Kaffee. Mit einer eigenen Tasse kann man ein wenig sparen. Zudem gibt es Automaten, diese funktionieren jedoch nur mit Karte."
+},{
+    "name": "NC-(Cafeteria)?",
+    "icon": "yellow",
+    "latitud": 51.4444,
+    "longitud": 7.26622,
+    "description": "Unbekannt ob es hier Kaffee gibt / ob aktuell geöffnet ist. 🦊 würde sich über eine Email freuen."
+},{
+    "name": "MA-(Cafeteria)?",
+    "icon": "yellow",
+    "latitud": 51.44463,
+    "longitud": 7.25923,
+    "description": "Unbekannt ob es hier Kaffee gibt / ob aktuell geöffnet ist. 🦊 würde sich über eine Email freuen."
+},{
+    "name": "HZO-Automat 🦊",
+    "icon": "green",
+    "latitud": 51.44492,
+    "longitud": 7.26226,
+    "description": "Im HZO stehen mehrere Automaten, jedoch sind diese auch die teuersten auf dem gesamten Campus."
+}];
